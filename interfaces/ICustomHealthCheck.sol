d24c4b6f2af3dc0e5c7e2c59ac29187cd1b5e492
borus-dev <smkbcross@gmail.com>
2023-02-21 19:12:25 +0100

    feat: added first draft of strategy

---
+++ interfaces/ICustomHealthCheck.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.6.12;
+
+interface ICustomHealthCheck {
+    function check(
+        address callerStrategy,
+        uint256 profit,
+        uint256 loss,
+        uint256 debtPayment,
+        uint256 debtOutstanding
+    ) external view returns (bool);
+
+    function setlossLimitRatio(uint256 _lossLimitRatio) external;
+}
